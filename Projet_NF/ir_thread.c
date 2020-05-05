e296e475f7b1b28f51c6f512ba015c43fcbbeb9c
Arcdarc <marc.favre@epfl.ch>
2020-05-05 09:06:05 +0200

    Pong a jour

--- Projet_NF/ir_thread.c
+++ Projet_NF/ir_thread.c
@@ -4,27 +4,27 @@
 #define	SECOND_IR_SENSOR		1
 #define	SEVENTH_IR_SENSOR		6
 #define EIGTH_IR_SENSOR			7
-#define REFLET_FEUILLE			50
+#define REFLET_FEUILLE			100
 
 _Bool aide_detection_ligne(){
-	if (get_calibrated_prox(FIRST_IR_SENSOR)>=REFLET_FEUILLE){
+	if (get_prox(FIRST_IR_SENSOR)>=REFLET_FEUILLE){
 		return 1;
 	}
-	if (get_calibrated_prox(EIGTH_IR_SENSOR)>=REFLET_FEUILLE){
+	if (get_prox(EIGTH_IR_SENSOR)>=REFLET_FEUILLE){
 		return 1;
 	}
 	return 0;
 }
 
 _Bool obstacle_droite(int reflet_obstacle){
-	if ((get_calibrated_prox(FIRST_IR_SENSOR)>=reflet_obstacle)||(get_calibrated_prox(SECOND_IR_SENSOR)>=reflet_obstacle)){
+	if ((get_prox(FIRST_IR_SENSOR)>=reflet_obstacle)||(get_prox(SECOND_IR_SENSOR)>=reflet_obstacle)){
 		return 1;
 	}
 	return 0;
 }
 
 _Bool obstacle_gauche(int reflet_obstacle){
-	if ((get_calibrated_prox(EIGTH_IR_SENSOR)>=reflet_obstacle)||(get_calibrated_prox(SEVENTH_IR_SENSOR)>=reflet_obstacle)){
+	if ((get_prox(EIGTH_IR_SENSOR)>=reflet_obstacle)||(get_prox(SEVENTH_IR_SENSOR)>=reflet_obstacle)){
 		return 1;
 	}
 	return 0;
